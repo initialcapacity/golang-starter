56ef7a9d9b93e90eab7176c513d5b02980b18a06
barinek <barinek@initialcapacity.io>
2022-06-03 04:05:26 -0600

    minor update to concourse pipeline
    Signed-off-by: barinek <barinek@initialcapacity.io>

--- cmd/dataanlayzer/analyzer_test.go
+++ cmd/dataanlayzer/analyzer_test.go
@@ -1,33 +1,23 @@
 package main
 
 import (
-  "fmt"
-  "github.com/stretchr/testify/assert"
-  "os"
-  "testing"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
 )
 
-func Test(t *testing.T) {
-  err := os.Setenv("POSTGRESQL_URL", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
-  if err != nil {
-    fmt.Println(err)
-    t.Fail()
-  }
-  go main()
-}
-
 func TestNew(t *testing.T) {
-  _ = os.Setenv("POSTGRESQL_URL", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
-  analyzer := newDataAnalyzer()
-  assert.NotNil(t, analyzer)
+	_ = os.Setenv("POSTGRESQL_URL", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
+	analyzer := newDataAnalyzer()
+	assert.NotNil(t, analyzer)
 }
 
 func TestMissingPostgres(t *testing.T) {
-  _ = os.Setenv("POSTGRESQL_URL", "")
-  defer func() {
-    if err := recover(); err == nil {
-      t.Fail()
-    }
-  }()
-  main()
+	_ = os.Setenv("POSTGRESQL_URL", "")
+	defer func() {
+		if err := recover(); err == nil {
+			t.Fail()
+		}
+	}()
+	main()
 }
